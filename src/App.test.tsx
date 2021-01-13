acbb8b69fca25bc67038dd5984cf9d0b8df601df
Théo Lavaux <lavauxtheo@gmail.com>
2021-01-13 04:59:58 +0100

    :sparkles: create a test snapshot

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+afterEach(cleanup);
+
+test('matches snapshot', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment).toMatchSnapshot();
 });
