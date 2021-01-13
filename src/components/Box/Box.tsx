2adede18a0c0ccb8439832fdb26ee79919570a87
Théo Lavaux <lavauxtheo@gmail.com>
2021-01-13 03:31:03 +0100

    :recycle: rename draggable component

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,8 +1,8 @@
 import React, { forwardRef } from 'react';
 import './Box.scss';
 
-const Dragabble = forwardRef<HTMLDivElement>((props, ref) => {
+const Box = forwardRef<HTMLDivElement>((props, ref) => {
   return <div id="box" ref={ref} />;
 });
 
-export { Dragabble as default };
+export { Box as default };
